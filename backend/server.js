aebd7339c2072e368f19a5d998bbc66471734813
rohitstudy70-arch <rohitstudy70@gmail.com>
2026-04-29 20:29:30 +0530

    Add new allowed origin for CORS

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ const app = express();
 const allowedOrigins = [
   process.env.FRONTEND_URL,
   "http://localhost:5173",
-  "http://127.0.0.1:5173"
+  "http://127.0.0.1:5173",
+  "https://arshi-enterprises.vercel.app"
+
 ].filter(Boolean);
 
 app.use(cors({
